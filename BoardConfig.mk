41f6c2301b3fcce369eba0a324e6ffb011c0ca5b
zskynet09 <hakimfbacc@gmail.com>
2024-12-01 21:19:04 +0530

    CPH2065: Add TWRP debugging flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,3 +109,6 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 
+# For debugging
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
