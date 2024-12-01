b64ada1ca1f337fdfe7d3dce448d9db4c2e3b415
zskynet09 <hakimfbacc@gmail.com>
2024-12-01 10:07:13 +0530

    CPH2065: Build fastboot service

--- device.mk
+++ device.mk
@@ -15,4 +15,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastbootd
 PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
