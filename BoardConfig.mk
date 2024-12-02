a078731bdc4e7fd736e239dcff5acd02fbbfd6b3
zskynet09 <hakimfbacc@gmail.com>
2024-12-02 20:48:38 +0530

    CPH2065: Add brightness path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,3 +98,5 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+
