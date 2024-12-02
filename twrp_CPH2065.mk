33c8f34d2a6a1e95a530e3f4c0afd787cec24f06
zskynet09 <hakimfbacc@gmail.com>
2024-12-02 21:46:26 +0530

    CPH2065: Inherit product base config

--- twrp_CPH2065.mk
+++ twrp_CPH2065.mk
@@ -6,6 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
